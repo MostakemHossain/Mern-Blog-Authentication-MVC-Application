4ea9dcc49e21fe6e56055da11a1bf3366852514c
MostakemHossain <mostakememon123@gmail.com>
2024-01-10 14:19:14 +0600

    :navbar created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 
-
+import { AiOutlineClose } from "react-icons/ai"
+import { Link } from "react-router-dom"
+import logo from "../assets/images/logo.png"
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+        <div className="container nav_container">
+            <Link to='/' className="nav_logo">
+                <img src={logo} alt="nav_logo"  />
+            </Link>
+            <ul className="nav_menu">
+                <li><Link to='/profile'>Mostakem</Link></li>
+                <li><Link to='/create'>Create Post</Link></li>
+                <li><Link to='/authors'>Authors</Link></li>
+                <li><Link to='/logout'>Logout</Link></li>
+            </ul>
+            <button className="nav_toggle_btn">
+                <AiOutlineClose/>
+            </button>
+
+        </div>
+
+    </nav>
   )
 }
 
